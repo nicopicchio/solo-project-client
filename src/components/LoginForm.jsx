7c1f43431c621b014de419cdbc0cbeb05614242f
Nico Picchio <nicolapicchio@gmail.com>
2022-05-16 16:04:07 +0100

    forms firing correctly

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,10 +1,38 @@
+import Logo from '../../src/assets/continental-logo.jpg';
+
 function LoginForm() {
+	const onFormChange = (event) => {
+		console.log(event.target.value);
+	};
+
+	const onFormSubmit = (event) => {
+		event.preventDefault();
+		console.log('form submitted');
+	};
+
 	return (
-		<form id='login-form'>
-			<input name='username' type='text' placeholder='Username' required />
-			<input name='password' type='password' placeholder='Password' required />
-			<button>Login</button>
-		</form>
+		<>
+			<div className='logo'>
+				<img src={Logo} alt='continental main logo' width='300px' />
+			</div>
+			<form id='login-form'>
+				<input
+					onChange={onFormChange}
+					name='username'
+					type='text'
+					placeholder='Username'
+					required
+				/>
+				<input
+					onChange={onFormChange}
+					name='password'
+					type='password'
+					placeholder='Password'
+					required
+				/>
+				<button onClick={onFormSubmit}>Login</button>
+			</form>
+		</>
 	);
 }
 
--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,15 +1,61 @@
+import Logo from '../../src/assets/continental-logo.jpg';
+
 function RegistrationForm() {
-  return (
-    
-    <form id="registration-form">
-      <input name='forename' type='text' placeholder='Forename' required/>
-      <input name='surname' type='text' placeholder='Surname' required/>
-      <input name='username' type='text' placeholder='Username' required/>
-      <input name='password' type='password' placeholder='Password' required/>
-      <input name='password' type='password' placeholder='Confirm password' required/>
-      <button>Register</button>
-    </form>
-  )
+	const onFormChange = (event) => {
+		console.log(event.target.value);
+	};
+
+	const onFormSubmit = (event) => {
+		event.preventDefault();
+		console.log('form submitted');
+	}
+
+	return (
+		<>
+			<div className='logo'>
+				<img src={Logo} alt='continental main logo' width='250px' />
+			</div>
+			<h1>Register Account</h1>
+			<form id='registration-form'>
+				<input
+					onChange={onFormChange}
+					name='forename'
+					type='text'
+					placeholder='Forename'
+					required
+				/>
+				<input
+					onChange={onFormChange}
+					name='surname'
+					type='text'
+					placeholder='Surname'
+					required
+				/>
+				<input
+					onChange={onFormChange}
+					name='username'
+					type='text'
+					placeholder='Username'
+					required
+				/>
+				<input
+					onChange={onFormChange}
+					name='password'
+					type='password'
+					placeholder='Password'
+					required
+				/>
+				<input
+					onChange={onFormChange}
+					name='password'
+					type='password'
+					placeholder='Confirm password'
+					required
+				/>
+				<button onClick={onFormSubmit}>Register</button>
+			</form>
+		</>
+	);
 }
 
-export default RegistrationForm
+export default RegistrationForm;
