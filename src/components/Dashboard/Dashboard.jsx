619082e2d6a448fe07009dba61a1bc45c6074f44
Nico Picchio <nicolapicchio@gmail.com>
2022-05-19 14:51:58 +0100

    sign out functionality implemented

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,27 +1,40 @@
 import '../Dashboard/Dashboard.css';
 import Logo from '../../../src/assets/continental-logo.jpg';
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
+  const navigate = useNavigate();
+  const signOut = (event) => {
+    event.preventDefault();
+    localStorage.clear()
+    navigate('/login')
+  }
+
 	return (
 		<>
 			<header>
-				<img src={Logo} alt='continental hotel logo' width='230px' />
+				<img
+					src={Logo}
+					alt='continental hotel logo'
+					width='230px'
+					id='logo-top-left'
+				/>
 				<div className='dashboard-title-container'>
 					<h2 className='dashboard-heading'>userLogged's Dashboard</h2>
 					<h2 className='dashboard-heading'>Available Balance: $ 3,000</h2>
 				</div>
-				<button id='logout-button'>Logout</button>
+				<button id='logout-button' onClick={signOut}>Logout</button>
 			</header>
 			<main>
 				<div className='main-left-container main-containers'>
-          <h2 className='main-container-heading'>Targets Accepted</h2>
-        </div>
+					<h2 className='main-container-heading'>Targets Accepted</h2>
+				</div>
 				<div className='main-middle-container main-containers'>
-          <h2 className='main-container-heading'>Fugitives</h2>
-        </div>
-        <div className='main-right-container main-containers'>
-          <h2 className='main-container-heading'>Targets Captured</h2>
-        </div>
+					<h2 className='main-container-heading'>Fugitives</h2>
+				</div>
+				<div className='main-right-container main-containers'>
+					<h2 className='main-container-heading'>Targets Captured</h2>
+				</div>
 			</main>
 		</>
 	);
