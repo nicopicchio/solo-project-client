d8538571f0d7fb897fd616df51a50e92228efe8e
Nico Picchio <nicolapicchio@gmail.com>
2022-05-19 13:27:51 +0100

    dashboard rough layout created

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -7,15 +7,21 @@ function Dashboard() {
 			<header>
 				<img src={Logo} alt='continental hotel logo' width='230px' />
 				<div className='dashboard-title-container'>
-					<h2 id='dashboard-title'>userLogged's Dashboard</h2>
-					<h2 id='dashboard-balance'>Available Balance: $ 3,000</h2>
+					<h2 className='dashboard-heading'>userLogged's Dashboard</h2>
+					<h2 className='dashboard-heading'>Available Balance: $ 3,000</h2>
 				</div>
 				<button id='logout-button'>Logout</button>
 			</header>
 			<main>
-				<div className='main-left-container'></div>
-				<div className='main-middle-container'></div>
-        <div className='main-right-container'></div>
+				<div className='main-left-container main-containers'>
+          <h2 className='main-container-heading'>Targets Accepted</h2>
+        </div>
+				<div className='main-middle-container main-containers'>
+          <h2 className='main-container-heading'>Fugitives</h2>
+        </div>
+        <div className='main-right-container main-containers'>
+          <h2 className='main-container-heading'>Targets Captured</h2>
+        </div>
 			</main>
 		</>
 	);
